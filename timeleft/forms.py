0fc90980459457a33f30890bef2772ef030c9606
Davide Sarra <davide.sarra@gmail.com>
2020-02-09 20:24:39 +0000

    Add time zone picker to form

--- timeleft/forms.py
+++ timeleft/forms.py
@@ -1,7 +1,13 @@
+import pendulum
 from flask_wtf import Form
+from wtforms.fields.core import SelectField
 from wtforms.fields.html5 import DateField, TimeField
 
 
 class TimestampForm(Form):
     date = DateField('DatePicker', format='%Y-%m-%d')
     time = TimeField('TimePicker', format='%H:%M')
+    time_zone = SelectField(
+        'TimeZonePicker',
+        choices=tuple(zip(pendulum.timezones, pendulum.timezones)),
+    )
--- timeleft/views.py
+++ timeleft/views.py
@@ -1,5 +1,6 @@
 import datetime as dt
 
+import pendulum
 from flask import render_template
 
 from timeleft.app import timeleft_app
@@ -11,5 +12,7 @@ def home():
     form = TimestampForm()
     if form.validate_on_submit():
         timestamp = dt.datetime.combine(form.date.data, form.time.data)
-        return timestamp.strftime('%Y-%m-%d %H:%M')
+        time_zone = form.time_zone.data
+        tz_timestamp = pendulum.instance(timestamp, tz=time_zone)
+        return str(tz_timestamp)
     return render_template('base.html', title='Time Left', form=form)
