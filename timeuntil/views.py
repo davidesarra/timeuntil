53bab341b1d62edf57a5a3c2fb9de951a69f7775
Davide Sarra <davide.sarra@gmail.com>
2020-02-11 12:08:11 +0000

    Parametrize render_template arguments

--- timeuntil/views.py
+++ timeuntil/views.py
@@ -9,12 +9,14 @@ from timeuntil.forms import TimestampForm
 
 @timeuntil_app.route('/', methods=['POST', 'GET'])
 def home():
+    title = 'Time Until'
+    template_name = 'base.html'
     form = TimestampForm()
     if form.validate_on_submit():
         timestamp = dt.datetime.combine(form.date.data, form.time.data)
         time_zone = form.time_zone.data
         tz_timestamp = pendulum.instance(timestamp, tz=time_zone)
         return render_template(
-            'base.html', title='Time Left', form=form, timestamp=tz_timestamp
+            template_name, title=title, form=form, timestamp=tz_timestamp
         )
-    return render_template('base.html', title='Time Left', form=form)
+    return render_template(template_name, title=title, form=form)
